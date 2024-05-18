0037a136f2a8c31492571135eb04aae1929efeba
Patrick Gronemeyer <pgronie@gmail.com>
2024-05-18 18:14:28 -0500

    ClassSPA2024

--- index.js
+++ index.js
@@ -1,4 +1,45 @@
-// add menu toggle to bars icon in nav bar
-document.querySelector(".fa-bars").addEventListener("click", () => {
-  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-});
+import Navigo from "navigo";
+import { camelCase } from "lodash";
+
+import { header, nav, main, footer } from "./components";
+import * as store from "./store";
+
+const router = new Navigo("/");
+
+function render(state = store.home) {
+  document.querySelector("#root").innerHTML = `
+    ${header(state)}
+    ${nav(store.nav)}
+    ${main(state)}
+    ${footer()}
+  `;
+  router.updatePageLinks();
+  
+  afterRender();
+}
+
+function afterRender() {
+  // add menu toggle to bars icon in nav bar
+  document.querySelector(".fa-bars").addEventListener("click", () => {
+    document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+  });
+}
+
+router
+.on({
+  "/": () => render(),
+  // Use object destructuring assignment to store the data and (query)params from the Navigo match parameter
+  // (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment)
+  // This reduces the number of checks that need to be performed
+  ":view": ({ data, params }) => {
+    // Change the :view data element to camel case and remove any dashes (support for multi-word views)
+    const view = data?.view ? camelCase(data.view) : "home";
+    if (view in store) {        
+      render(store[view]);
+    } else {
+      render(store.viewNotFound);
+      console.log(`View ${view} not defined`);
+    }
+  },
+})
+.resolve();
---
+++ store/order.js
@@ -0,0 +1,4 @@
+export default {
+    header: "Your Order",
+    view: "order"
+  };
