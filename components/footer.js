64fee0b6748703e32a57919beda4583541602f20
Patrick Gronemeyer <pgronie@gmail.com>
2024-05-21 21:37:18 -0500

    classspa debug

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,6 @@
 import html from "html-literal";
 
-export default (state) => html`
+export default state => html`
   <footer>
     &copy; 2020 <a href="https://savvycoders.com/">Savvy Coders</a>
   </footer>
